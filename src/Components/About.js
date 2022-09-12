8fbeb28c9ce4091b81e8dabff358630d1df68a20
MATTHEW STATON <43524360+Matt525@users.noreply.github.com>
2022-09-12 15:26:45 -0400

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -19,7 +19,7 @@ export default function About() {
 								<h2 className="mb-4" style={{fontWeight: '400', fontSize: '3em',}}>Let Me
 									Introduce
 									myself</h2>
-								<p className="lead">I am a 29 year old <span className="blockquote">self built</span> front end engineer with a power drive and self motivation to bring the customers ideas to life. 
+								<p className="lead">I am a <span className="blockquote">self built</span> front end engineer with a power drive and self motivation to bring the customers ideas to life. 
 									With my positive, forward thinking mindset and attention to every detail, I only strive to deliver what can be my best performance 
 									with every project that I do. With that said, I am always learning and welcome the opportunity to work with you and continue to grow
 									my real world knowledge so that I can make the user & customer experience better with each new project. 
